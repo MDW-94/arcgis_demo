488051c3100fecf7dfd9d0ed358d683bb75b9ec8
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-01-07 14:47:15 +0000

    needs a bit more work

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <>
+      <h1>Avon and Somerset Police Data</h1>
       <WebMap/>
+      <h2>UI - Switch between different datasets? Select which data is to be displayed through the map? Toggle ID references through React?</h2>
     </>
   )
 }
