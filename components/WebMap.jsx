2bd2c7f83752b45c110a290d918a692c5a6fa2e1
Matthew David Wasylko <matthew.wasylko@gmail.com>
2024-01-07 16:45:19 +0000

    most of the map switch features have now been added

--- components/WebMap.jsx
+++ components/WebMap.jsx
@@ -1,18 +1,23 @@
 import { useEffect, useRef } from "react";
+import { switchMap } from "../lib/app";
 
-const WebMap = () => {
+const WebMap = ({mapSelection}) => {
 
     const viewRef = useRef();
 
     useEffect(() => {
         import("../lib/app").then(function(app){
+            const selectMapId = switchMap(mapSelection);
+            app.webMap.portalItem.id = selectMapId;
             app.initialize(viewRef.current)
         })
-    })
+    }, [mapSelection])
 
     return ( 
     <div className="viewDiv" ref={viewRef}/>
     );
 }
+
+
  
 export default WebMap;
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react"
 import WebMap from "../components/WebMap"
 
 function App() {
 
+  const [mapSelection, setMapSelection] = useState(0);
+
+  const handleMapChange = (event) => {
+    setMapSelection(event.target.value);
+    console.log(event.target.value)
+    console.log(mapSelection)
+  }
+
   return (
     <>
       <h1>Avon and Somerset Police Data</h1>
-      <WebMap/>
-      <h2>UI - Switch between different datasets? Select which data is to be displayed through the map? Toggle ID references through React?</h2>
+      <WebMap mapSelection={mapSelection}/>
+      <div>
+        <h2>UI</h2>
+        <select onChange={handleMapChange}>
+          <option value="0">Avon and Somerset Map</option>
+          <option value="1">Crime and Poverty UK</option>
+        </select>
+      </div>
+      
     </>
   )
 }
 
-export default App
+export default App;
